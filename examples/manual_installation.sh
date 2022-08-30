e597a5e5181f3447492e4155f5c573ddebdc77ce
Rain <Rainyan@users.noreply.github.com>
2022-08-30 19:21:53 +0300

    Create manual_installation.sh

---
+++ examples/manual_installation.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/Rainyan/discord-bot-ntpug
+cd discord-bot-ntpug
+python -m pip install --upgrade pip
+
+# Before continuing, consider virtualizing the
+# environment below to keep things clean.
+# https://pipenv.pypa.io/en/latest/
+
+# Install requirements
+pip install -r requirements.txt
+
+# Edit config.yml and/or set env vars as required
+
+# Run the bot!
+python bot.py
