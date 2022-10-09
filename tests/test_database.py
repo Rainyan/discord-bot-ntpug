4cf9b432ca52d4d1d61e403e28a730ffc4bae58f
Rain <Rainyan@users.noreply.github.com>
2022-10-09 18:42:41 +0300

    tests wip

--- tests/test_database.py
+++ tests/test_database.py
@@ -15,19 +15,35 @@ from pug import database
 DriverBase = TypeVar("DriverBase", bound="database.DbDriver")
 
 
+def test_dbdrivers_value(dbdrivers: str) -> None:
+    if dbdrivers.casefold() != "all":
+        drivers = ("sqlite3", "postgres")
+        assert all(
+            [
+                x in [y for y in drivers]
+                for x in dbdrivers.casefold().split(",")
+            ]
+        )
+
+
 class TestPostgres:
     @pytest.mark.asyncio
     async def test_create_db(self, dbdrivers: str) -> None:
+        if (
+            dbdrivers.casefold() != "all"
+            and "postgres" not in dbdrivers.casefold().split(",")
+        ):
+            pytest.skip(reason="Skipped by --dbdrivers")
         print(f"Hello from {self.__class__.__name__}: {dbdrivers}")
         db = database.Postgres(
             dbname=os.getenv("PYTEST_DB_DBNAME"),
             user=os.getenv("PYTEST_DB_USER"),
             password=os.getenv("PYTEST_DB_PASSWORD"),
             host=os.getenv("PYTEST_DB_HOST"),
-            port=int(os.getenv("PYTEST_DB_PORT"))
+            port=int(os.getenv("PYTEST_DB_PORT")),
         )
+        # First, drop any existing tables so that we have a clean slate
         async with db(1) as driver:
-            # Drop all tables
             await driver._execute(
                 """
 DO $$ DECLARE
@@ -41,6 +57,7 @@ LOOP
 END LOOP;
 END $$;"""
             )
+        async with db(1) as driver:
             res = await driver._execute(
                 "SELECT * FROM INFORMATION_SCHEMA.TABLES;"
             )
@@ -50,14 +67,21 @@ END $$;"""
 class TestSqlite3:
     @pytest.mark.asyncio
     async def test_create_db(self, dbdrivers: str) -> None:
+        if (
+            dbdrivers.casefold() != "all"
+            and "sqlite3" not in dbdrivers.casefold().split(",")
+        ):
+            pytest.skip(reason="Skipped by --dbdrivers")
         print(f"Hello from {self.__class__.__name__}: {dbdrivers}")
 
         db = database.Sqlite3(database=os.getenv("PYTEST_DB_DBNAME"))
+        # First, drop any existing tables so that we have a clean slate
         async with db(1) as driver:
             await driver._execute(
                 """SELECT 'DROP TABLE ' || name || ';' from sqlite_master
     WHERE type = 'table';"""
             )
+        async with db(1) as driver:
             res = await driver._execute(
                 "SELECT name FROM sqlite_schema WHERE type='table' ORDER BY name;"
             )
